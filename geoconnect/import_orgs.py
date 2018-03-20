d62541ed491d0c98dcfd7486407c9c2034b1c328
Adam Preston <adam.m.preston@gmail.com>
2018-03-20 10:05:55 -0600

    Export JSON instead

--- geoconnect/import_orgs.py
+++ geoconnect/import_orgs.py
@@ -6,20 +6,22 @@ import requests
 import os
 import pandas as pd
 import numpy as np
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-u", "--uid",
                     help="this uid of admin0 in the destination DHIS2 system where you are importing org units from.")
 parser.add_argument("-n", "--name", help='this name of the admin0 in geoconnect you want to pull in org units from')
-
+parser.add_argument("-a", "--attribute", help='this is the geoconnect attribute uid in DHIS2')
 args = parser.parse_args()
 
-if len(sys.argv) < 2:
+if len(sys.argv) < 3:
     parser.print_help()
     sys.exit(1)
 
 ADMIN0_UID = str(args.uid)
 ADMIN0_NAME = str(args.name)
+ATTRIBUTE_UID = str(args.attribute)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 API_KEY = os.environ.get("API_KEY")
@@ -52,7 +54,13 @@ regions = np.unique(orgunits['admin1'].values)
 for region in regions:
 
     region_uid = "ORG" + '%08d' % org_counter
-    row = {"name": region, "uid": region_uid, "parent": ADMIN0_UID}
+    parent_id = {"id": ADMIN0_UID}
+
+    row = {"name": region,
+           "shortName": region,
+           "openingDate": "1970-01-01T00:00:00.000",
+           "id": region_uid,
+           "parent": parent_id}
     export.append(row)
     org_counter = org_counter + 1
 
@@ -65,7 +73,12 @@ for region in regions:
     for district in districts:
 
         district_uid = "ORG" + '%08d' % org_counter
-        row = {"name": district, "uid": district_uid, "parent": region_uid}
+        parent_id = {"id": region_uid}
+        row = {"name": district,
+               "shortName": district,
+               "openingDate" : "1970-01-01T00:00:00.000",
+               "id": district_uid,
+               "parent": parent_id}
         export.append(row)
         org_counter = org_counter + 1
 
@@ -75,13 +88,27 @@ for region in regions:
         admin2_df = admin2_df.loc[admin2_df['admin3'].notnull()]
         subdistricts = np.unique(admin2_df[['admin3']].values)
 
-        for subdistrict in subdistricts:
+        for index, row in admin2_df.iterrows():
+
+            attr_value = {"id": ATTRIBUTE_UID}
+            attribute_data = {"value": row['geoconnect_id'], "attribute": attr_value}
+            attributeValues = [attribute_data]
+
             subdistrict_uid = "ORG" + '%08d' % org_counter
-            row = {"name": subdistrict, "uid": subdistrict_uid, "parent": district_uid}
+
+            parent_id = {"id": district_uid}
+
+            row = {"name": row['admin3'],
+                   "shortName": row['admin3'],
+                   "openingDate": "1970-01-01T00:00:00.000",
+                   "id": subdistrict_uid,
+                   "parent": parent_id,
+                   "attributeValues": attributeValues}
+
             export.append(row)
             org_counter = org_counter + 1
 
-# Export to a CSV like this: name,uid,code,parent
-print("name,uid,code,parent")
-for n in export:
-    print(n['name']+","+n['uid']+",,"+n['parent'])
+
+orgunits = {"organisationUnits": export}
+jstr = json.dumps(orgunits, ensure_ascii=False, indent=4)
+print(jstr)
